9f2285d2505b286a54d5e6217e5644c97b76e3d4
iz3dvq <iz3dvq@users.noreply.github.com>
2014-01-14 11:09:40 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -17,7 +17,7 @@
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '222', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= 'FF0000', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
